719c68b4018c933de9798322e16cc772151bee0f
Edmar Amorim <trovador@hotmail.com.br>
2024-11-03 12:09:12 -0300

    Create: scan-name-say Hello.java
    Scan name of user and say Hello

---
+++ JAVA Beginners Coding Challenge/scan-name-say Hello.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner keyboard = new Scanner(System.in);
+    	String name = keyboard.nextLine();
+      
+      System.out.println("Hello "+ name);
+  }
+}
