1e6f23cda9f443015f9c3594f24f58ec9f9d60ff
Edmar Amorim <trovador@hotmail.com.br>
2024-11-06 11:17:39 -0300

    Update: print-1-10-numbers.java
    Print numbers 1 to 10 each number in a separate line

--- JAVA Beginners Coding Challenge/print-1-10-numbers.java
+++ JAVA Beginners Coding Challenge/print-1-10-numbers.java
@@ -1,3 +1,21 @@
+/******************************************************************************
+
+Print numbers 1 to 10 each number in a separate line
+Max Score: 5 | Difficulty: Not Specified
+Print numbers from 1 to 10 using any loop of your choice. Following is the expected output:
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+*******************************************************************************/
 import java.util.*;
 
 public class Main {
