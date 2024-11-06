5df493cf6aaa7f992e8e0f6b4cbcba74545671e7
Edmar Amorim <trovador@hotmail.com.br>
2024-11-06 11:02:57 -0300

    Update and rename: Print Hello World.java to print-hello-world.java
    Print 'Hello World'

---
+++ JAVA Beginners Coding Challenge/print-hello-world.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+Print 'Hello World'
+Max Score: 1 | Difficulty: Not Specified
+Write a program to print the following to the console
+
+Hello World
+
+*******************************************************************************/
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+  }
+}
