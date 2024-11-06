db542b2434c2e2b808b215fa78a575483a6a6bc9
Edmar Amorim <trovador@hotmail.com.br>
2024-11-06 11:12:50 -0300

    Update: number-positive-or-negative.java
    Print numbers 1 to 10 each number in a separate line

--- JAVA Beginners Coding Challenge/number-positive-or-negative.java
+++ JAVA Beginners Coding Challenge/number-positive-or-negative.java
@@ -1,15 +1,28 @@
+/******************************************************************************
+
+Print numbers 1 to 10 each number in a separate line
+Max Score: 5 | Difficulty: Not Specified
+Print numbers from 1 to 10 using any loop of your choice. Following is the expected output:
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+*******************************************************************************/
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-       Scanner keyboard = new Scanner(System.in);
-       int number = keyboard.nextInt();
-       keyboard.close();
-
-      if (number < 0) {
-          System.out.println("negative");
-      } else {
-          System.out.println("positive");
+      
+      for (int i=1; i<=10; i++) {
+        System.out.println(i);
       }
-    }
+  }
 }
