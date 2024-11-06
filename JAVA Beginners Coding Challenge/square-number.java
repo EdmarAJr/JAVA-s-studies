2d23a54eaa6eaab60d6e655aa987531a647bfb6e
Edmar Amorim <trovador@hotmail.com.br>
2024-11-06 11:16:23 -0300

    Update: square-number.java
    Square of a number

--- JAVA Beginners Coding Challenge/square-number.java
+++ JAVA Beginners Coding Challenge/square-number.java
@@ -1,3 +1,15 @@
+/******************************************************************************
+
+Square of a number
+Max Score: 5 | Difficulty: Not Specified
+Take a number as input and calculate the square of that number
+
+Input:
+4
+Output:
+16
+
+*******************************************************************************/
 import java.util.*;
 
 public class Main {
