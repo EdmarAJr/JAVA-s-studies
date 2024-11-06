1e6d5aae7bda26cc5ca30797d89a3dcd25cd1fba
Edmar Amorim <trovador@hotmail.com.br>
2024-11-06 11:12:23 -0300

    Update: multiply-two-numbers.java
    Leap year or not ?

--- JAVA Beginners Coding Challenge/multiply-two-numbers.java
+++ JAVA Beginners Coding Challenge/multiply-two-numbers.java
@@ -1,3 +1,16 @@
+/******************************************************************************
+
+Leap year or not ?
+Max Score: 5 | Difficulty: Not Specified
+Take a year as input and tell whether or not it's a leap year.
+Print yes If it's a leap year and print no if it's not a leap year.
+
+Input:
+2000
+Output:
+yes
+
+*******************************************************************************/
 import java.util.*;
 
 public class Main {
