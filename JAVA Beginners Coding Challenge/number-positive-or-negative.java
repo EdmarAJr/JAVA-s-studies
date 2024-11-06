e195e4408ea920c207213781ae386d2b60b47389
Edmar Amorim <trovador@hotmail.com.br>
2024-11-06 11:22:49 -0300

    Update: number-positive-or-negative.java
    Given number Positive or Negative

--- JAVA Beginners Coding Challenge/number-positive-or-negative.java
+++ JAVA Beginners Coding Challenge/number-positive-or-negative.java
@@ -1,28 +1,27 @@
 /******************************************************************************
 
-Print numbers 1 to 10 each number in a separate line
+Given number Positive or Negative
 Max Score: 5 | Difficulty: Not Specified
-Print numbers from 1 to 10 using any loop of your choice. Following is the expected output:
+Take a number as input and tell its positive or negative
 
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
+Input:
+-42
+Output:
+negative
 
 *******************************************************************************/
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-      
-      for (int i=1; i<=10; i++) {
-        System.out.println(i);
+       Scanner keyboard = new Scanner(System.in);
+       int number = keyboard.nextInt();
+       keyboard.close();
+
+      if (number < 0) {
+          System.out.println("negative");
+      } else {
+          System.out.println("positive");
       }
-  }
+    }
 }
