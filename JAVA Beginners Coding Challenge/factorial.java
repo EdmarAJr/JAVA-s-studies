5adad2b4d54b75b80faf9b8d20ff6a9d1599fc8a
Edmar Amorim <trovador@hotmail.com.br>
2024-11-06 11:30:31 -0300

    Create: factorial.java
    Factorial of a number

---
+++ JAVA Beginners Coding Challenge/factorial.java
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Factorial of a number
+Max Score: 7 | Difficulty: Not Specified
+Take a number as an input and print the factorial of that number.
+
+Input:
+5
+Output:
+120
+
+*******************************************************************************/
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner keyboard = new Scanner(System.in);
+      int number = keyboard.nextInt();
+      keyboard.close();
+      int fatorial=1;
+      
+      while(number > 0){
+        fatorial*=number;
+        number--;
+      }
+
+      System.out.println(fatorial);
+  }
+}
