fca8fbeee913a1cdfa4c6f58b859626e0c2f9fae
Edmar Amorim <trovador@hotmail.com.br>
2024-11-18 08:25:48 -0300

    Create: exchange-values-variation.java
    Enter 2 numbers a,b - exchange their values without using third variable and print them

---
+++ Basic Arithmetic Operations/exchange-values-variation.java
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Question 12
+Max Score: 2 | Difficulty: Not Specified
+Enter 2 numbers a,b - exchange their values without using third variable and print them
+
+Constraints:
+0 < a,b < 1000
+
+Input:
+Two integers - a,b each in a new line
+Output:
+Integers a,b after being reversed without using a third varibles
+
+Example:
+Input:
+244
+566
+
+Output:
+566
+244
+
+*******************************************************************************/
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner keyboard = new Scanner(System.in);
+      int a = keyboard.nextInt();
+      int b = keyboard.nextInt();
+      keyboard.close();
+      
+  
+      System.out.println(b);
+      System.out.println(a);
+    }
+}
