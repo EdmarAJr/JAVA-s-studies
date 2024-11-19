7e58a0e42b05832663810f2f59e039134fe048e3
Edmar Amorim <trovador@hotmail.com.br>
2024-11-19 09:59:30 -0300

    Create: print-100-natural-numbers.java

---
+++ Loops - 1/print-100-natural-numbers.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Question 1
+Max Score: 2 | Difficulty: Not Specified
+Print the first 100 natural numbers
+
+Input:
+No input is provided.
+Output:
+First 100 natural numbers are printed. Each integer in a new line.
+
+*******************************************************************************/
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      int n=1;
+      while(n<=100){
+        System.out.println(n);
+        n++;
+      }
+      
+  }
+}
